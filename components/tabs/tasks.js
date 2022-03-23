ff4f3965cd9d7aef849895e25803d966242c176f
Nick <nickwentworth123@gmail.com>
2022-03-23 00:33:34 -0400

    added task interaction functions

--- components/tabs/tasks.js
+++ components/tabs/tasks.js
@@ -1,19 +1,23 @@
 import { getFormattedDueDateString } from '../../lib/utility.js';
 import styles from './tasks.module.css';
 
-export default function Tasks({ tasks }) {
+export default function Tasks({ tasks, taskFunctions }) {
     return (
         <div className={styles.tasks}>
-            {tasks.map((task) => <TaskPanel task={task} />)}
+            <button onClick={taskFunctions.add.bind(this, { name: 'New homework', due: 'March 23 2022', class: 'Chinese' })}>add task</button>
+            <button onClick={taskFunctions.remove.bind(this, tasks.length-1)}>remove task</button>
+            <button onClick={taskFunctions.edit.bind(this, tasks.length-1, { name: 'Changed homework', due: 'March 24 2022', class: 'Chinese' })}>change task</button>
+
+            {tasks.map((task, index) => <TaskPanel key={index} task={task} index={index} />)}
         </div>
     )
 }
 
-function TaskPanel({ task }) {
+function TaskPanel({ task, index }) {
     return (
         <div className={styles.taskPanel}>
             <div className={styles.taskHeader}>
-                <h3>{task.name}</h3>
+                <h3>{index} - {task.name}</h3>
                 <h4>{task.class}</h4>
             </div>
 
--- pages/[tab].js
+++ pages/[tab].js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Sidebar from '../components/sidebar.js';
 import { getAllTabNames, getAllTabData } from '../lib/tabs.js';
 import styles from './[tab].module.css';
@@ -7,14 +8,46 @@ import Dashboard from '../components/tabs/dashboard.js';
 import Notes from '../components/tabs/notes.js';
 import Tasks from '../components/tabs/tasks.js';
 
-export default function Tab({ tabData, currentTab, tasks }) {
+// TODO - probably have to add custom date with exact times being optional, only date required
+// TEMP - just testing for now
+let initialTasks = [
+    { name: 'Homework A', class: 'Math', due: 'March 24, 2022 23:59', description: 'math book page 123', progress: 0},
+    { name: 'Homework B', class: 'Science', due: 'March 27, 2022 12:00', description: 'lab 3 pdf on canvas', progress: 0},
+    { name: 'Homework C', due: 'April 12, 2022 12:00:00', progress: 0}
+]
+// ----
+
+export default function Tab({ tabData, currentTab }) {
+    const [tasks, setTasks] = useState(initialTasks);
+    
+    // adds a task to the end of the array
+    function addTask(newTask) {
+        setTasks([...tasks, newTask]);
+    }
+
+    // removes a task at the given index
+    function removeTask(index) {
+        setTasks(tasks.filter((task, i) => {
+            return index != i;
+        }))
+    }
+
+    // edits the task at the given index to the given new task
+    function editTask(index, newTask) {
+        setTasks(tasks.map((task, i) => {
+            return (index != i) ? task : newTask;
+        }))
+    }
+
+    const taskFunctions = { add: addTask, remove: removeTask, edit: editTask }; // given to pages to interact with tasks
+
     return (
         <div className={styles.page}>
             <Sidebar tabData={tabData} currentTab={currentTab} />
             
             {currentTab == 'dashboard' && <Dashboard />}
             {currentTab == 'notes' && <Notes />}
-            {currentTab == 'tasks' && <Tasks tasks={tasks} />}
+            {currentTab == 'tasks' && <Tasks tasks={tasks} taskFunctions={taskFunctions} />}
         </div>
     )
 }
@@ -31,20 +64,10 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
     let tabData = getAllTabData();
 
-    // TODO - probably have to add custom date with exact times being optional, only date required
-    // TEMP - just testing for now
-    let tasks = [
-        { name: 'Homework A', class: 'Math', due: 'March 24, 2022 23:59', description: 'math book page 123', progress: 0},
-        { name: 'Homework B', class: 'Science', due: 'March 27, 2022 12:00', description: 'lab 3 pdf on canvas', progress: 0},
-        { name: 'Homework C', due: 'April 12, 2022 12:00:00', progress: 0}
-    ]
-    // ----
-
     return {
         props: {
             tabData,
-            currentTab: params.tab,
-            tasks
+            currentTab: params.tab
         }
     }
 }
