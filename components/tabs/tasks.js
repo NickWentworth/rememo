ea98eb22ba63f10c1532243a76cb8ba2479beb4c
Nick <nickwentworth123@gmail.com>
2022-03-24 22:45:20 -0400

    added task editing bars

--- components/tabs/tasks.js
+++ components/tabs/tasks.js
@@ -1,31 +1,61 @@
+import { useState } from 'react';
 import { getFormattedDueDateString } from '../../lib/utility.js';
 import styles from './tasks.module.css';
+
+const iconSize = 30;
 
 export default function Tasks({ tasks, taskFunctions }) {
     return (
         <div className={styles.tasks}>
             <button onClick={taskFunctions.add.bind(this, { name: 'New homework', due: 'March 23 2022', class: 'Chinese' })}>add task</button>
-            <button onClick={taskFunctions.remove.bind(this, tasks.length-1)}>remove task</button>
-            <button onClick={taskFunctions.edit.bind(this, tasks.length-1, { name: 'Changed homework', due: 'March 24 2022', class: 'Chinese' })}>change task</button>
 
-            {tasks.map((task, index) => <TaskPanel key={index} task={task} index={index} />)}
+            {tasks.map((task, index) => <TaskPanel
+                key={index}
+                task={task} 
+                index={index} 
+                taskFunctions={taskFunctions}
+            />)}
         </div>
     )
 }
 
-function TaskPanel({ task, index }) {
+function TaskPanel({ task, index, taskFunctions }) {
+    const [showEditBar, setShowEditBar] = useState(false);
+
     return (
-        <div className={styles.taskPanel}>
-            <div className={styles.taskHeader}>
-                <h3>{index} - {task.name}</h3>
-                <h4>{task.class}</h4>
+        <div className={styles.taskPanel} onMouseEnter={setShowEditBar.bind(this, true)} onMouseLeave={setShowEditBar.bind(this, false)}>
+            <div className={styles.taskPanelBody} style={{ borderRadius: showEditBar ? '10px 0px 0px 10px' : '10px' }}>
+                    <div className={styles.taskPanelHeader}>
+                        <h3>{index} - {task.name}</h3>
+                        <h4>{task.class}</h4>
+                    </div>
+
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp;{getFormattedDueDateString(task.due)}</p>
+
+                    {task.description && <hr />}
+                    
+                    <p>{task.description}</p>
             </div>
 
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;{getFormattedDueDateString(task.due)}</p>
-
-            {task.description && <hr />}
-            
-            <p>{task.description}</p>
+            <div className={styles.taskEditBar} style={{ opacity: showEditBar ? '100%' : '0%' }}>
+                <input
+                    className={styles.editButton}
+                    type='image'
+                    src='/images/icons/edit.svg'
+                    width={iconSize}
+                    height={iconSize} 
+                    // TODO -- add edit task window
+                    onClick={() => alert('Editing Task ' + index)} 
+                />
+                <input
+                    className={styles.deleteButton}
+                    type='image'
+                    src='/images/icons/delete.svg'
+                    width={iconSize}
+                    height={iconSize} 
+                    onClick={taskFunctions.delete.bind(this, index)} 
+                />
+            </div>
         </div>
     )
 }
--- pages/[tab].js
+++ pages/[tab].js
@@ -10,10 +10,11 @@ import Tasks from '../components/tabs/tasks.js';
 
 // TODO - probably have to add custom date with exact times being optional, only date required
 // TEMP - just testing for now
+let lorum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et faucibus dolor, nec tempor nibh. Nullam ac iaculis est, eu sagittis orci. Aenean et lectus vulputate, egestas arcu vitae, pellentesque diam. Nullam sed diam venenatis, varius leo et, lacinia enim. Nulla sed eros at turpis egestas semper. Donec orci ipsum, lacinia eu pellentesque vitae, viverra eu lectus. Sed ex libero, pharetra nec varius aliquet, luctus sit amet leo.'
 let initialTasks = [
     { name: 'Homework A', class: 'Math', due: 'March 24, 2022 23:59', description: 'math book page 123', progress: 0},
     { name: 'Homework B', class: 'Science', due: 'March 27, 2022 12:00', description: 'lab 3 pdf on canvas', progress: 0},
-    { name: 'Homework C', due: 'April 12, 2022 12:00:00', progress: 0}
+    { name: 'Homework C', description: lorum, due: 'April 12, 2022 12:00:00', progress: 0}
 ]
 // ----
 
@@ -22,13 +23,17 @@ export default function Tab({ tabData, currentTab }) {
     
     // adds a task to the end of the array
     function addTask(newTask) {
-        setTasks([...tasks, newTask]);
+        setTasks([...tasks, newTask].sort((a, b) => {
+            return Date.parse(a.due) - Date.parse(b.due);
+        }))
     }
 
-    // removes a task at the given index
-    function removeTask(index) {
+    // deletes a task at the given index
+    function deleteTask(index) {
         setTasks(tasks.filter((task, i) => {
             return index != i;
+        }).sort((a, b) => {
+            return Date.parse(a.due) - Date.parse(b.due);
         }))
     }
 
@@ -36,10 +41,12 @@ export default function Tab({ tabData, currentTab }) {
     function editTask(index, newTask) {
         setTasks(tasks.map((task, i) => {
             return (index != i) ? task : newTask;
+        }).sort((a, b) => {
+            return Date.parse(a.due) - Date.parse(b.due);
         }))
     }
 
-    const taskFunctions = { add: addTask, remove: removeTask, edit: editTask }; // given to pages to interact with tasks
+    const taskFunctions = { add: addTask, delete: deleteTask, edit: editTask }; // given to pages to interact with tasks
 
     return (
         <div className={styles.page}>
