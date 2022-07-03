ae4f40ee2ba791e9c7b2fa005de695d46ee47332
Nick <nickwentworth123@gmail.com>
2022-07-03 16:27:51 -0400

    improved 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,37 @@
-import Link from "next/link";
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Link from 'next/link';
 import styles from './pages.module.css'
 
 export default function Custom404() {
-    return (
-        <div className={styles.custom404 + ' fill'}>
-            <h1>404 - This page could not be found</h1>
+    const router = useRouter();
 
-            {/* TODO - return to dashboard once its created */}
-            <Link href='/tasks'>
-                <button>Back to Rememo</button>
-            </Link>
-        </div>
+    return (
+        <>
+            <Head>
+                <title>Page not found • Rememo</title>
+            </Head>
+
+            <div className={'fill'}>
+                <div className={styles.custom404}>
+                    <h1>This page does not exist</h1>
+
+                    <hr />
+
+                    <div className={styles.custom404Message}>
+                        <p>The requested URL</p>
+                        <p>{router.asPath}</p>
+                        <p>could not be found</p>
+                    </div>
+
+                    <hr />
+
+                    {/* TODO - return to dashboard once its created */}
+                    <Link href='/tasks'>
+                        <button>Back to Rememo</button>
+                    </Link>
+                </div>
+            </div>
+        </>
     )
 }
