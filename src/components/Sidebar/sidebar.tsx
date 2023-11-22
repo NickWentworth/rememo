9552ef765566dd50f8598a266cbdac6ff5f3b80c
Nick <nickwentworth123@gmail.com>
2023-11-21 22:27:05 -0500

    added button links to sidebar

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -3,6 +3,7 @@
 import { useReducer } from 'react';
 import styles from './sidebar.module.css';
 import { Calendar, Courses, Dashboard, Hamburger, Logo, Tasks } from '../icons';
+import Link from 'next/link';
 
 const ICON_SIZE = 32;
 const LOGO_SIZE = ICON_SIZE + 16;
@@ -15,7 +16,9 @@ export function Sidebar() {
             {/* Logo */}
             <div className={styles.logo}>
                 <Logo color='accent' size={LOGO_SIZE} />
-                {expanded && <h1 className={styles.logoText}>Rememo</h1>}
+                {expanded && (
+                    <h1 className={`${styles.logoText} logoFont`}>Rememo</h1>
+                )}
             </div>
 
             <hr />
@@ -24,21 +27,25 @@ export function Sidebar() {
             <div className={styles.links}>
                 <PageLink
                     name='Dashboard'
+                    to='/dashboard'
                     icon={<Dashboard color='white' size={ICON_SIZE} />}
                     expanded={expanded}
                 />
                 <PageLink
                     name='Tasks'
+                    to='/tasks'
                     icon={<Tasks color='white' size={ICON_SIZE} />}
                     expanded={expanded}
                 />
                 <PageLink
                     name='Courses'
+                    to='/courses'
                     icon={<Courses color='white' size={ICON_SIZE} />}
                     expanded={expanded}
                 />
                 <PageLink
                     name='Calendar'
+                    to='/calendar'
                     icon={<Calendar color='white' size={ICON_SIZE} />}
                     expanded={expanded}
                 />
@@ -58,18 +65,23 @@ export function Sidebar() {
 }
 
 type PageLinkProps = {
+    // display name of link
     name: string;
+    // routing location of link
+    to: string;
+    // icon to display
     icon: React.ReactNode;
-    // TODO: redirect to a page
-    // to: string;
+    // is the sidebar expanded?
     expanded: boolean;
 };
 
 function PageLink(props: PageLinkProps) {
     return (
-        <button className={`${styles.pageLink} ${styles.button}`}>
-            {props.icon}
-            {props.expanded && <h4>{props.name}</h4>}
-        </button>
+        <Link className={styles.pageLink} href={props.to}>
+            <button className={`${styles.pageLinkButton} ${styles.button}`}>
+                {props.icon}
+                {props.expanded && <h3>{props.name}</h3>}
+            </button>
+        </Link>
     );
 }
