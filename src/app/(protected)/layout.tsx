b46e9a88e122bdf80342defbee094be3032b6582
Nick <nickwentworth123@gmail.com>
2024-01-24 15:35:32 -0500

    relocated pages into a protected route group

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,9 @@
 import Providers from '@/providers';
 import Sidebar from '@/components/Sidebar';
-import './global.css';
+import '../global.css';
+
+// Pages in the (protected) layout group require the user to be signed into an active session.
+// They also automatically include a sidebar component along with the page content
 
 type LayoutProps = {
     children: React.ReactNode;
