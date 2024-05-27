89ce0ea249ef0859ef7fda4783307474f2b878fd
Nick <nickwentworth123@gmail.com>
2024-05-27 16:00:12 -0400

    removed unused files

--- src/components/Button/index.ts
+++
@@ -1,8 +0,0 @@
-// general-use button component
-export { default } from './Button';
-
-// specialized buttons
-import AddButton from './AddButton';
-import FilterButton from './FilterButton';
-
-export { AddButton, FilterButton };
